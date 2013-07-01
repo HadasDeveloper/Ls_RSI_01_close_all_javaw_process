de31d940647497ae769c8aeea3cf57cb4de73821
Kapelushnik ALLA <Allakape@gmail.com>
2013-07-01 16:42:41 +0300

    1

---
+++ Ls_RSI_01_close_all_javaw_process/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ls_RSI_01_close_all_javaw_process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcessesByName("javaw");
+            foreach (Process process in processes)
+            {
+                Console.WriteLine(process.Id);
+                process.Kill();
+            }
+        }
+    }
+}
